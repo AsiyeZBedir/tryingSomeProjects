a7330ac27355c8c861842219340799a641c6e82f
Asiye <116940286+AsiyeZBedir@users.noreply.github.com>
2023-09-30 18:59:17 +0300

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+     animal = input("Choose an animal: ")
+     verb = input("Choose a verb: ")
+     adjective = input("Choose an adjective: ")
+     food = input("Choose a food: ")
+     place = input("Choose a place: ")
+
+     story = f"The {animal} likes to {verb} in the {adjective} garden while eating {food} in the {place}."
+
+     print(story)
